9a72fd249736eda57f30576ef082fb72ab338c58
Andrei <iandy1731@gmail.com>
2022-03-21 21:51:07 +0300

    deployed on the gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Result from './components/Result';
 import { store } from './store';
@@ -10,13 +10,10 @@ const App = () => {
         <div className="wraper">
             <div className="container">
                 <Provider store={store}>
-                    <Router>
                         <Switch>
                             <Route exact path="/" component={Home} />
-                            <Route exact path="/result" component={Result} />
-                            <Redirect to="/redirect" />
+                            <Route exact path="/result" component={Result} />    
                         </Switch>
-                    </Router>
                 </Provider>
             </div>
         </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {Provider} from "react-redux";
-import {store} from "./store";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <HashRouter>
+            <App />
+        </HashRouter>
     </Provider>,
-  document.querySelector(".app__body")
+    document.querySelector(".app__body")
 );
 
